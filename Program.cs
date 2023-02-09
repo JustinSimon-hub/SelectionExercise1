40569c0122052f308a388cfb557ccbe671f2aeae
Justin Simon <justinsimon@Justins-MBP.lan>
2023-02-08 17:59:02 -0800

    Finished Exercise 1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace SelectionStatementExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Can you guess what my favorite number is?");
+           
+           var favNumber = 23;
+             var userInput = int.Parse(Console.ReadLine());
+
+
+
+            if (favNumber > userInput)
+            {
+                Console.WriteLine("Too low!");
+
+            }
+            else if ( favNumber < userInput)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+    
+                
+        }
+    }
+}
